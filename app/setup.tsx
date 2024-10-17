4c2d506c3a51276824b60e8daf6150597399b83b
Sanmandresy <98638893+Sanmandresy@users.noreply.github.com>
2024-10-18 00:08:15 +0300

    style: beautify setup screen style

--- app/setup.tsx
+++ app/setup.tsx
@@ -1,26 +1,32 @@
-import { Anchor, Paragraph, View, XStack } from "tamagui";
+import {
+	Button,
+	Input,
+	Paragraph,
+	SizableText,
+	View,
+	XStack,
+	YStack,
+} from "tamagui";
+import { LogIn } from "@tamagui/lucide-icons";
 
 export default function Setup() {
 	return (
-		<View flex={1} alignItems="center" justifyContent="center">
-			<XStack gap="$2">
-				<Paragraph ta="center">Made by</Paragraph>
-				<Anchor
-					col="$blue10"
-					href="https://twitter.com/natebirdman"
-					target="_blank"
-				>
-					@natebirdman,
-				</Anchor>
-				<Anchor
-					color="$purple10"
-					href="https://github.com/tamagui/tamagui"
-					target="_blank"
-					rel="noreferrer"
-				>
-					give it a ⭐️
-				</Anchor>
+		<View flex={1} alignItems="center" justifyContent="center" gap="$10">
+			<XStack gap="$2" ai="center" scale={0} opacity={0} animation={"zoomIn"}>
+				<SizableText size="$9">Bienvenue sur</SizableText>
+				<SizableText color={"$red10"} fontWeight={"bold"} size="$9">
+					Poketa
+				</SizableText>
 			</XStack>
+			<YStack gap="$5">
+				<Paragraph size={"$5"}>
+					Pour commencer, veuillez saisir votre pseudo
+				</Paragraph>
+				<Input />
+			</YStack>
+			<Button fontSize={"$5"} backgroundColor={"$red10"} color="white">
+				Commencer <LogIn color="white" size={"$1"} />
+			</Button>
 		</View>
 	);
 }
--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,7 +1,17 @@
 import { config as configBase } from "@tamagui/config/v3";
+import { createAnimations } from "@tamagui/animations-react-native";
 import { createTamagui } from "tamagui";
 
-export const config = createTamagui(configBase);
+export const config = createTamagui({
+	...configBase,
+	animations: createAnimations({
+		zoomIn: {
+			toValue: 1,
+			duration: 700,
+			damping: 200,
+		},
+	}),
+});
 
 export default config;
 
